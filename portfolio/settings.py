f0a792189cfe5df59bf34ef9f28ec9c3c5d4819e
Rishav Parihar <rishavparihar81@gmail.com>
2025-08-28 11:38:01 +0530

    update my project

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.Whitenoise.Middleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
